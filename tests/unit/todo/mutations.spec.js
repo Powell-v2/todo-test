d5244eb79afc20e580ede3621a83de6fbbaef83e
Paul Ye <oatto.paul@gmail.com>
2020-05-15 09:43:18 +0200

    Make sure existing unit tests are passing

--- tests/unit/todo/mutations.spec.js
+++ tests/unit/todo/mutations.spec.js
@@ -15,6 +15,10 @@ beforeEach(() => {
   });
 });
 
+function prefixTodoMutation(mutation) {
+  return `todo/${mutation}`;
+}
+
 const task = {
   msg: "Create TODO test project",
   detail: "I must do this so I can test applicants",
@@ -23,7 +27,7 @@ const task = {
 
 describe("TodoMutations", () => {
   test("Add new todo", () => {
-    store.commit(mutations.ADD_TODO, task);
+    store.commit(prefixTodoMutation(mutations.ADD_TODO), task);
 
     expect(store.state.todo.todoList).toContain(task);
   });
@@ -31,13 +35,13 @@ describe("TodoMutations", () => {
   test("Set todo completion state", () => {
     store.state.todo.todoList.push(task);
 
-    store.commit(mutations.SET_STATE, {
+    store.commit(prefixTodoMutation(mutations.SET_STATE), {
       index: 0,
       complete: true
     });
     expect(store.state.todo.todoList[0].complete).toBe(true);
 
-    store.commit(mutations.SET_STATE, {
+    store.commit(prefixTodoMutation(mutations.SET_STATE), {
       index: 0,
       complete: false
     });
@@ -47,7 +51,7 @@ describe("TodoMutations", () => {
   test("Remove TODO", () => {
     store.state.todo.todoList.push(task);
 
-    store.commit(mutations.REMOVE_TODO, 0);
+    store.commit(prefixTodoMutation(mutations.REMOVE_TODO), 0);
 
     expect(store.state.todo.todoList).toEqual([]);
   });
